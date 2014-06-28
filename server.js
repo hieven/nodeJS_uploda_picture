8c635dfc4f2fbb9fcf5fa6eb5cf3f3cb974cb27d
Even Chang <kiki44552002@gmail.com>
2014-06-28 21:37:30 +0800

    first node.js practice

--- server.js
+++ server.js
@@ -3,16 +3,11 @@ var url = require("url");
 
 function start(route, handle){
 
-	function onRequest(request, response){
+    function onRequest(request, response){
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " +pathname+ " received.");
-
-		route(handle, pathname);
-
-		response.writeHead(200, {"Content-Type" : "text/plain"});
-		response.write("hello world");
-		response.end();
-	}
+        route(handle, pathname, response, request);
+    }
 
 	http.createServer(onRequest).listen(8888);
 	console.log("Server has started!");
